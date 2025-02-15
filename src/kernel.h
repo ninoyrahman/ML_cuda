f52fed3b3e96dd6076b5bb9e6c2328d5873f64ab
ninoy rahman <ninoyrahman@gmail.com>
2025-02-15 19:28:43 +0100

    reformat

--- src/kernel.h
+++ src/kernel.h
@@ -1,3 +1,6 @@
+#include "cublas_v2.h"
+#include <iostream>
+
 // Kernel that print vector
 __global__ void print_vector_device(const float *a, int n){
 
@@ -45,5 +48,41 @@ __global__ void mat_mul_transpose(const float *a, const float *b, float *c, int 
     tmp += a[i * c_row + row] * b[i * c_col + col];
   }
   c[row * c_col + col] = tmp;
+
+}
+
+void compute(const float *matA, const float *matB, float *matC, float *vecB, float *vecO, int N1, int N2, int N3){
+
+  cublasStatus_t status;
+  cublasHandle_t handle;
+    
+  float alpha = 1.0f;
+  float beta = 0.0f;
+
+  cublasCreate(&handle);
+
+  status = cublasSgemm(handle,
+    CUBLAS_OP_N, CUBLAS_OP_N,
+    N1, N2, N3,
+    &alpha,
+    matA, N1,
+    matB, N3,
+    &beta,
+    matC, N1);
+
+  if (status != CUBLAS_STATUS_SUCCESS) 
+    printf("cublasSgemm returned error code %d\n", status);
+
+  // gpu matrix vector addition
+  alpha = 1.0f;
+  status = cublasSger(handle, 
+    N1, N2,
+    &alpha,
+    vecB, 1,
+    vecO, 1,
+    matC, N1);
+
+  if (status != CUBLAS_STATUS_SUCCESS) 
+    printf("cublasSger returned error code %d\n", status);
 
 }
--- src/main_cublas.cu
+++ src/main_cublas.cu
@@ -5,6 +5,8 @@
 #include "util.h"
 #include "kernel.h"
 #include "cublas_v2.h"
+
+#define PROF  0
  
 // main routine that executes on the host
 int main(void){
@@ -15,18 +17,11 @@ int main(void){
   // varify gpu calculation
   bool isVarify = true;
 
+#if PROF == 0
   // profile variable
   float et;
   cudaEvent_t start, stop;
-
-  // cublas handle
-  cublasStatus_t status;
-  cublasHandle_t handle;
-  cublasCreate(&handle);
-
-  // scaling factor
-  float alpha;
-  float beta;
+#endif  
 
   // matrix and vector size
   const int N1 = 4096; // matC row/matA row number
@@ -64,8 +59,10 @@ int main(void){
   ////////////////////////////////////initialization////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+#if PROF == 0
   // initialize performance profile variables
   cudaEventCreate(&start); cudaEventCreate(&stop);
+#endif
 
   // print matrix size
   std::cout << " A matrix dims = (" << N1 << ", " << N3 << ")" << std::endl;
@@ -81,68 +78,34 @@ int main(void){
   setvalue_vector(vecO_h, N2, 1.0f);
   
   // copy data from host to device
+#if PROF == 0
   cudaEventRecord(start);
+#endif
   cudaMemcpy(matA_d, matA_h, size_matA, cudaMemcpyHostToDevice); // copy matA to device
   cudaMemcpy(matB_d, matB_h, size_matB, cudaMemcpyHostToDevice); // copy matB to device
   cudaMemcpy(vecB_d, vecB_h, size_vecB, cudaMemcpyHostToDevice); // copy vecB to device
   cudaMemcpy(vecO_d, vecO_h, size_vecO, cudaMemcpyHostToDevice); // copy vecO to device
+#if PROF == 0
   cudaEventRecord(stop); cudaEventSynchronize(stop); cudaEventElapsedTime(&et, start, stop);
   std::cout << "time elapsed(copy to device) = " << et << std::endl;
+#endif
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////
   ////////////////////////////////////computation///////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////////////////////////////////////  
-  
-  // gpu matrix multiplication
-  cudaEventRecord(start);
-  alpha = 1.0f;
-  beta = 0.0f;
-  status = cublasSgemm(handle,
-    CUBLAS_OP_N, CUBLAS_OP_N,
-    N1, N2, N3,
-    &alpha,
-    matA_d, N1,
-    matB_d, N3,
-    &beta,
-    matC_d, N1);
-  cudaEventRecord(stop); cudaEventSynchronize(stop); cudaEventElapsedTime(&et, start, stop);
-  std::cout << "time elapsed(mat x mat) = " << et << std::endl;
 
-  if (status != CUBLAS_STATUS_SUCCESS) 
-    printf("cublasSgemm returned error code %d\n", status);
-
-  if (isVarify){
-    // Retrieve result from device and store it in host array
-    cudaEventRecord(start);
-    cudaMemcpy(matC_h, matC_d, size_matC, cudaMemcpyDeviceToHost);
-    cudaEventRecord(stop); cudaEventSynchronize(stop); cudaEventElapsedTime(&et, start, stop);
-    std::cout << "time elapsed(copy from device) = " << et << std::endl;
-
-    // cpu matrix multiplication 
-    // copy matC from device to host before calling varify routine
-    mat_mul_varify(matA_h, matB_h, matC_h, N1, N2, N3);
-  }
-
-  // gpu matrix vector addition
-  cudaEventRecord(start);
-  alpha = 1.0f;
-  status = cublasSger(handle, 
-    N1, N2,
-    &alpha,
-    vecB_d, 1,
-    vecO_d, 1,
-    matC_d, N1);
-  cudaEventRecord(stop); cudaEventSynchronize(stop); cudaEventElapsedTime(&et, start, stop);
-  std::cout << "time elapsed(mat + vec) = " << et << std::endl;
-
-  if (status != CUBLAS_STATUS_SUCCESS) 
-    printf("cublasSger returned error code %d\n", status);
+  // forward propagation
+  compute(matA_d, matB_d, matC_d, vecB_d, vecO_d, N1, N2, N3);
 
   // Retrieve result from device and store it in host array
+#if PROF == 0
   cudaEventRecord(start);
+#endif
   cudaMemcpy(matC_h, matC_d, size_matC, cudaMemcpyDeviceToHost);
+#if PROF == 0
   cudaEventRecord(stop); cudaEventSynchronize(stop); cudaEventElapsedTime(&et, start, stop);
-  std::cout << "time elapsed(copy from device) = " << et << std::endl;    
+  std::cout << "time elapsed(copy from device) = " << et << std::endl;
+#endif
 
   if (isVarify){
     // cpu matrix multiplication and vector addition 
